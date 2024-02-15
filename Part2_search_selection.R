0c792d403f18a461bb233a572bca343b9ee78b4d
clementinecttn <clementine.cottineau@iscpif.fr>
2024-02-15 14:45:45 +0100

    prepare common script with allocated sections and package names

---
+++ Part2_search_selection.R
@@ -0,0 +1,15 @@
+### Code to exemplify Part 2 with Xiaoxia's query
+
+
+# Section 1: Retrieve references from WoS and Scopus (Shuyu) 
+library(wosr)
+library(rscopus)
+
+# Section 2: Combine tables, deduplicate references and summarise (Kyri?) 
+library(revtools)
+
+# Section 3: Suggest new keywords (Clementine) 
+library(litsearchr) 
+
+# Section 4: build PRISMA figure (Xiaoxia)
+library(PRISMA2020)
